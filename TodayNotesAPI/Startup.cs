5e0213b34220e307c8cea3118da7b2986872d012
Ivan Ledinich <ivan.ledinich@gmail.com>
2019-05-27 16:07:09 +1000

    Walking skeleton

--- TodayNotesAPI/Startup.cs
+++ TodayNotesAPI/Startup.cs
@@ -29,6 +29,7 @@ namespace TodayNotesAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddCors();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
@@ -44,8 +45,8 @@ namespace TodayNotesAPI
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
-            app.UseHttpsRedirection();
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
+            // app.UseHttpsRedirection();
             app.UseMvc();
         }
     }
--- TodayNotesSPA/src/app/app.component.ts
+++ TodayNotesSPA/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'TodayNotesSPA';
+  notes: any;
+
+  constructor(private http: HttpClient) {
+    this.http.get('http://localhost:5000/api/values').subscribe(resp => {
+      this.notes = resp;
+      console.log(this.notes);
+    });
+  }
 }
