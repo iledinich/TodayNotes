a042d8544d36ce5acc4fcf57ac447074ab94014d
Ivan Ledinich <ivan.ledinich@gmail.com>
2020-04-08 17:01:48 +1000

    Next feature

--- TodayNotesAPI/Helpers/Theme.cs
+++ TodayNotesAPI/Helpers/Theme.cs
@@ -1,5 +1,6 @@
 namespace TodayNotesAPI.Helpers
 {
+    //Future feature: Implement dark / light theme
     public class Theme
     {
         public static readonly string DefaultColor = "#d4d3d3";
